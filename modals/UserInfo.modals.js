a08e8eaa3b79c20dc2b6d014b642e761d0b5fdd5
vijendrsingh <vijendra@cleankoding.com>
2024-09-11 12:56:09 +0530

    connecting the mongodb and established the user info more

--- modals/UserInfo.modals.js
+++ modals/UserInfo.modals.js
@@ -1,15 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  linearUserId: { type: String, required: true, unique: true },
   accessToken: { type: String, required: true },
-  refreshToken: { type: String },
-  tokenExpiresAt: { type: Date },
-  email: { type: String },
-  name: { type: String },
-  avatarUrl: { type: String },
-  organizationId: { type: String },
-  provider: { type: String, default: "linear" },
 });
 
 // Create a User model
