9779f0270681e09485e124fc07ddd054a842e019
vijendrsingh <vijendra@cleankoding.com>
2024-09-11 18:12:20 +0530

    connecting the mongodb and established the user info more

--- modals/TasksDetails.modals.js
+++ modals/TasksDetails.modals.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   userEmail: { type: String, required: true },
   accessTokenGet: { type: Date, required: true },
 });
