1317956fdc933dc9b8a7c46372f8fb0fb768da59
vijendrsingh <vijendra@cleankoding.com>
2024-09-18 10:26:45 +0530

    Adding the cors configuaration

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const { connectionToDB } = require("./config/db");
 const { slackRouter } = require("./routes/Slack.routes");
 const { linearRoutes } = require("./routes/Linear.routes");
 const { clickupRoutes } = require("./routes/Clickup.routes");
-
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "https://app.getaligned.work",
+      "https://stage-app.getaligned.work",
+      "https://mail.google.com",
+      "https://api.getaligned.work",
+      "https://extension.getaligned.work",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+  })
+);
 app.use("/", slackRouter);
 app.use("/", linearRoutes);
 app.use("/", clickupRoutes);
+
 app.get("/", async (req, res) => {
   res.send("home page for slack");
 });
